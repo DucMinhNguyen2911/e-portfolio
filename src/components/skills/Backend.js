bfaf64d6a2cc1a29b66d5e5c0b258a7b75c62da6
Nguyen Duc Minh K17HL <minhndhe170485@fpt.edu.vn>
2023-06-06 02:51:16 +0700

    setup gh-pages

---
+++ src/components/skills/Backend.js
@@ -0,0 +1,60 @@
+import React from "react";
+import "./skills.css";
+
+function Backend() {
+    return (
+        <div className="skills__content">
+            <h3 className="skills__title">Backend Developer</h3>
+            <div className="skills__box">
+                <div className="skills__group">
+                    <div className="skills__data">
+                        <i class="bx bx-badge-check"></i>
+                        <div>
+                            <h3 className="skills__name">Java</h3>
+                            <span className="skills__level">Basic</span>
+                        </div>
+                    </div>
+                    <div className="skills__data">
+                        <i class="bx bx-badge-check"></i>
+                        <div>
+                            <h3 className="skills__name">SQL Server</h3>
+                            <span className="skills__level">Basic</span>
+                        </div>
+                    </div>
+                    <div className="skills__data">
+                        <i class="bx bx-badge-check"></i>
+                        <div>
+                            <h3 className="skills__name">ASP.NET Core</h3>
+                            <span className="skills__level">Basic</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="skills__group">
+                    <div className="skills__data">
+                        <i class="bx bx-badge-check"></i>
+                        <div>
+                            <h3 className="skills__name">MySQL</h3>
+                            <span className="skills__level">Basic</span>
+                        </div>
+                    </div>
+                    <div className="skills__data">
+                        <i class="bx bx-badge-check"></i>
+                        <div>
+                            <h3 className="skills__name">JSP</h3>
+                            <span className="skills__level">Basic</span>
+                        </div>
+                    </div>
+                    <div className="skills__data">
+                        <i class="bx bx-badge-check"></i>
+                        <div>
+                            <h3 className="skills__name">Servlet</h3>
+                            <span className="skills__level">Basic</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Backend;
---
+++ src/components/work/Data.js
@@ -0,0 +1,23 @@
+import Work1 from "../../assets/work1.png"
+
+export const projectsData = [
+    {
+      id: 1,
+      image: Work1,
+      title: "Phone Card Store",
+      category: "web",
+      link: "http://dum1.us-east-1.elasticbeanstalk.com",
+    },
+  ];
+
+  export const projectsNav =[
+    {
+        name: "all",
+    },
+    {
+        name: "web",
+    },
+    {
+        name: 'app',
+    },
+  ]
